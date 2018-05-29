9df1c894e742b9686fbd11b3023aa98df451ba69
salesforceplatform <jweijters@salesforce.com>
2018-05-29 12:02:49 +0200

    Added button

--- db_init.sql
+++ db_init.sql
@@ -5,3 +5,6 @@ CREATE TABLE "session" (
 )
 WITH (OIDS=FALSE);
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+
+postgres://enzogevzzczgmc:c233287879ba092797f73375c58351334b1d969662671180df4e4739c541d2a5@ec2-79-125-6-160.eu-west-1.compute.amazonaws.com:5432/dc0fal1f7fb9fk
